3b9d77e55d7917eec74a070e121d311aff38075f
Georgemburu <33799338+Georgemburu@users.noreply.github.com>
2024-06-16 18:47:47 +0300

    Update rv1Predictor.js

--- rv1Predictor.js
+++ rv1Predictor.js
@@ -1891,7 +1891,7 @@ async function getFullDocument({
 
 const OBJ = window.obj;
 window.ReactNativeWebView.postMessage("Window.obj")
-window.ReactNativeWebView.postMessage(OBJ)
+window.ReactNativeWebView.postMessage(JSON.stringify({OBJ:OBJ}))
 getFullDocument({
     season_number: OBJ.season_number,
     game_round_name: OBJ.game_round_name,
