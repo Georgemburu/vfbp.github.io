0262feab7b88ad8067c7bea7e22c63a11d331edc
Georgemburu <33799338+Georgemburu@users.noreply.github.com>
2025-02-22 19:26:38 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@
 
 // const SERVER_DOMAIN = "http://rnbty-78-159-114-104.a.free.pinggy.link";
 const SERVER_DOMAIN = "https://reliably-whole-jennet.ngrok-free.app";//"http://78.159.114.104:3008";
+const BETPAWA_HTFT_BETTOR_SERVER_DOMAIN = SERVER_DOMAIN;//"http://78.159.114.104:3019";
 const ACTIVATION_CODES = {
     BETPAWA: {
         ONE_X_TWO_ODDS_PREDICTOR: {
@@ -66,7 +67,23 @@ async function fetchBetpawaPickedBetsJSON_WithPostReq(){
     // /**
     //  * @type {import("../../../../API_SERVER/API_Betpawa_Server/routes/predictors/one_x_two_odds_predictor_routes/api_v1.1.1.3/index").API_Get_OneXTwoPredictorStrategyAccuracies_JSON_Response_Interface}
     // */
-    const respJson = await performJSONFetch(`${SERVER_DOMAIN}/api/get_bets_data`,{
+    const respJson = await performJSONFetch(`${SERVER_DOMAIN}/BTTS_Bettor/api/get_bets_data`,{
+        method:"POST",
+        body: ""
+    }); 
+    // console.log({respJson})
+    return respJson; 
+}
+
+async function fetchBetpawa_HTFT_PickedBetsJSON_WithPostReq(){
+    const headers = new Headers();
+    headers.append("Access-Control-Allow-Origin","*");
+     
+    // const API_VERSION_NAME = "1.1.1.3"
+    // /**
+    //  * @type {import("../../../../API_SERVER/API_Betpawa_Server/routes/predictors/one_x_two_odds_predictor_routes/api_v1.1.1.3/index").API_Get_OneXTwoPredictorStrategyAccuracies_JSON_Response_Interface}
+    // */
+    const respJson = await performJSONFetch(`${BETPAWA_HTFT_BETTOR_SERVER_DOMAIN}/HTFT_Bettor/api/get_bets_data`,{
         method:"POST",
         body: ""
     }); 
@@ -79,7 +96,8 @@ async function fetchBetpawaPickedBetsJSON_WithPostReq(){
 /**
  * @typedef {{
  *  performJSONFetch: performJSONFetch,
- *  fetchBetpawaPickedBetsJSON_WithPostReq: fetchBetpawaPickedBetsJSON_WithPostReq
+ *  fetchBetpawaPickedBetsJSON_WithPostReq: fetchBetpawaPickedBetsJSON_WithPostReq,
+ *  fetchBetpawa_HTFT_PickedBetsJSON_WithPostReq: fetchBetpawa_HTFT_PickedBetsJSON_WithPostReq
  * }} WindowWithUtils
 */
 //  *  fetchUsersJSON: fetchUsersJSON,
